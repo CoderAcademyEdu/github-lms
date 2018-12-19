a10e8524e90f7ec2814ec62d3a97b1d7794b20c7
dijonmusters <jonathon.d.meyers@gmail.com>
2018-12-20 08:37:57 +1100

    ENHANCE: Implement caching in lesson component

--- client/src/pages/lesson.js
+++ client/src/pages/lesson.js
@@ -1,32 +1,50 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import fm from 'front-matter';
+import frontmatter from 'front-matter';
 import ReactMarkdown from 'react-markdown/with-html';
+import isEqual from 'lodash/isEqual';
+import Loading from '../components/loading';
 
 class Lesson extends Component {
-  state = { lesson: null };
+  state = {
+    body: null,
+    fm: null
+  };
 
   componentDidMount() {
     const { REACT_APP_COHORT: cohort } = process.env;
     const { module, lesson } = this.props.match.params;
-    axios.get(`/api/${cohort}/modules/${module}/${lesson}`)
+    const url = `/api/${cohort}/modules/${module}/${lesson}`;
+    const cachedContent = JSON.parse(localStorage.getItem(url));
+    if (cachedContent && cachedContent.body && cachedContent.attributes) {
+      this.setState({ body: cachedContent.body, fm: cachedContent.attributes });
+    }
+    axios.get(url)
       .then(({ data }) => {
-        const content = fm(data);
-        this.setState({ lesson: content.body });
+        const content = frontmatter(data);
+        const { body, attributes: fm } = content;
+        if (!cachedContent
+          || body !== cachedContent.body
+          || !isEqual(fm, cachedContent.attributes)) {
+          localStorage.setItem(url, JSON.stringify(content));
+          this.setState({ body, fm });
+        }
       })
       .catch(error => console.log(error));
   }
 
   render() {
-    const { lesson } = this.state;
-    return (
+    const { body, fm } = this.state;
+    return body && fm ? (
       <>
+        <h1>{fm.title}</h1>
+        { fm.lecture_video && <video controls="controls" src={`${fm.lecture_video}&html5=true`} /> }
         <ReactMarkdown
-          source={lesson}
+          source={body}
           escapeHtml={false}
         />
       </>
-    );
+    ) : <Loading />;
   }
 }
 
