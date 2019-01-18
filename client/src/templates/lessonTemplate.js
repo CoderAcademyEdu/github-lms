b2559bb4fd7b417edc440a506fdfbecaf6157bef
dijonmusters <jonathon.d.meyers@gmail.com>
2019-01-18 17:16:29 +1100

    NEW: Add download code feature for lessons

--- client/src/templates/lessonTemplate.js
+++ client/src/templates/lessonTemplate.js
@@ -1,33 +1,84 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
 import YouTubePlayer from 'react-player/lib/players/YouTube';
 import ReactMarkdown from 'react-markdown/with-html';
+import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import FileSaver from 'file-saver';
+import axios from 'axios';
+import { convertSpacesToUnderscores } from '../utils/pathToDisplay';
 
-const renderFrontMatter = (fm) => {
-  const { title, lecture_video } = fm;
-  return (
-    <>
-      <Helmet>
-        <title>{process.env.REACT_APP_COHORT} - {title}</title>
-      </Helmet>
-      { title && <h1>{title}</h1> }
-      { lecture_video &&
-          <YouTubePlayer
-            url={lecture_video}
-            controls
-            width="100%"
-            config={{
-              youtube: {
-                playerVars: { showinfo: 1 }
-              }
-            }}
-          />
-      }
-    </>
-  );
-}
+const Download = styled.button`
+  background-color: rgba(0, 0, 0, 0.05);
+  padding: 10px;
+  color: #333;
+  margin: 5px;
+  font-size: 12px;
+  cursor: pointer;
+`
+
+const Flex = styled.div`
+  display: flex;
+`
 
 const LessonTemplate = (props) => {
+
+  const downloadFile = (download) => {
+    const { url: fileUrl, name } = download;
+    const { REACT_APP_COHORT: cohort } = process.env;
+    const { module } = props.match.params;
+    const url = `/api/${cohort}/download/${module}/${fileUrl}`;
+    const options = {
+      responseType: 'blob'
+    }
+    axios.get(url, options)
+      .then(resp => {
+        const fileName = `${convertSpacesToUnderscores(name)}.zip`;
+        FileSaver.saveAs(resp.data, fileName);
+      })
+      .catch(err => console.log(err));
+  }
+
+  const renderDownloads = (downloads) => {
+    return (
+      <Flex>
+        {
+          downloads.map(download => (
+            <Download key={download.name} onClick={() => downloadFile(download)}>
+              <FontAwesomeIcon icon="download" /> { download.name }
+            </Download>
+          ))
+        }
+      </Flex>
+    );
+  }
+
+  const renderFrontMatter = (fm) => {
+    const { title, lecture_video, downloads } = fm;
+    return (
+      <>
+        <Helmet>
+          <title>{process.env.REACT_APP_COHORT} - {title}</title>
+        </Helmet>
+        { title && <h1>{title}</h1> }
+        { lecture_video &&
+            <YouTubePlayer
+              url={lecture_video}
+              controls
+              width="100%"
+              config={{
+                youtube: {
+                  playerVars: { showinfo: 1 }
+                }
+              }}
+            />
+        }
+        { downloads && renderDownloads(downloads) }
+      </>
+    );
+  }
+
   const { fm, body } = props;
   return (
     <>
@@ -42,4 +93,4 @@ const LessonTemplate = (props) => {
   );
 }
 
-export default LessonTemplate;
+export default withRouter(LessonTemplate);
--- routes/protected.js
+++ routes/protected.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const github = require('../utils/github');
 const { isAuthenticated, hasRole, isEnrolled } = require('../utils/auth');
 const db = require('../models/index');
+const request = require('request');
 
 router.use(isAuthenticated);
 router.use(isEnrolled);
@@ -49,6 +50,21 @@ router.get('/:cohort/challenges/:module/:challenge', (req, res) => {
   github.get(url)
     .then(({ data }) => res.send(data))
     .catch(error => res.send(error));
+});
+
+router.get('/:cohort/code/:module/:file', (req, res) => {
+  const { GITHUB_BASE_URL: githubUrl, GITHUB_TOKEN: token } = process.env;
+  const { cohort, module, file } = req.params;
+  const url = `${githubUrl}/${cohort}/code/${module}/${file}`;
+  const options = {
+    headers: {
+      'Authorization': `token ${token}`,
+      'Accept': 'application/vnd.github.v4.raw',
+      'User-Agent': 'request'
+    }
+  }
+  req.pipe(request(url, options))
+    .pipe(res);
 });
 
 module.exports = router;
